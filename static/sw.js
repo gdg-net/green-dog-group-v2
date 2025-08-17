c3e7c9eedad383f8bd992fc78c847902eb583f06
gdg-net <lucasisnew6@gmail.com>
2025-08-17 10:20:59 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,17 +1,27 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.sw.js');
+importScripts("/uv/config.js");
+importScripts("/uv/worker.js");
+importScripts("/uv/bundle.js");
+importScripts("/uv/extra/config.js");
+importScripts(__uv$config.sw || "/uv/sw.js");
 
 const uv = new UVServiceWorker();
+const dynamic = new Dynamic();
 
-async function handleRequest(event) {
-    if (uv.route(event)) {
+const userKey = new URL(location).searchParams.get("userkey");
+self.dynamic = dynamic;
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    (async () => {
+      if (await dynamic.route(event)) {
+        return await dynamic.fetch(event);
+      }
+
+      if (event.request.url.startsWith(`${location.origin}/a/`)) {
         return await uv.fetch(event);
-    }
-    
-    return await fetch(event.request)
-}
+      }
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest(event));
+      return await fetch(event.request);
+    })(),
+  );
 });
