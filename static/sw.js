d9d6746bea74719a7923099d6871035368ab378c
gdg-net <lucasisnew6@gmail.com>
2025-08-17 10:14:15 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,21 +1,17 @@
-var cacheName = 'TIWcog';
-var filesToCache = [
-  '/js/sw.js'
-];
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('/uv/uv.sw.js');
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
+const uv = new UVServiceWorker();
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+async function handleRequest(event) {
+    if (uv.route(event)) {
+        return await uv.fetch(event);
+    }
+    
+    return await fetch(event.request)
+}
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event));
 });
