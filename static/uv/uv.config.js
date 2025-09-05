457015e3acf75c15b45936c277be8da1df6b7b6a
gdg-net <lucasisnew6@gmail.com>
2025-09-05 10:09:30 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-  prefix: "/service/uv/",
+  prefix: "/static/uv/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
