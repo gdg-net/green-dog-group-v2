f70e3368460c26ed272a80d15c7f74224677315f
gdg-net <lucasisnew6@gmail.com>
2025-09-13 09:27:27 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,38 +1,72 @@
-"use strict";
-/**
- * @type {HTMLFormElement}
- */
-const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
-const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
-const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
-const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
-const errorCode = document.getElementById("uv-error-code");
+async function init() {
+    try {
+        const connection = new BareMux.BareMuxConnection("/baremux/worker.js");
+        const wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+        if (localStorage.getItem("transport") == "epoxy") {
+            if (await connection.getTransport() !== "/epoxy/index.mjs") {
+                await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+                console.log("Using Epoxy transport. Wisp URL is: " + wispUrl);
+            }
+        }
+        else if (localStorage.getItem("transport") == "libcurl") {
+            if (await connection.getTransport() !== "/libcurl/index.mjs") {
+                await connection.setTransport("/libcurl/index.mjs", [{ wisp: wispUrl }]);
+                console.log("Using Libcurl transport. Wisp URL is: " + wispUrl);
+            }
+        }
+        else {
+            localStorage.setItem("transport", "epoxy");
+            if (await connection.getTransport() !== "/epoxy/index.mjs") {
+                await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+                console.log("Using Epoxy transport. Wisp URL is: " + wispUrl);
+            }
+        }
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+    } catch (err) {
+        console.error("An error occurred while setting up baremux:", err);
+    }
+}
 
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-    
 
-  }
+if (!localStorage.getItem("proxy")) {
+    localStorage.setItem("proxy", "uv");
+}
 
-  const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+const scramjet = new ScramjetController({
+    prefix: "/scram/service/",
+    files: {
+        wasm: "/scram/scramjet.wasm.wasm",
+        worker: "/scram/scramjet.worker.js",
+        client: "/scram/scramjet.client.js",
+        shared: "/scram/scramjet.shared.js",
+        sync: "/scram/scramjet.sync.js"
+    },
+    flags: {
+        "serviceworkers": false,
+        "syncxhr": false,
+        "naiiveRewriter": false,
+        "strictRewrites": true,
+        "rewriterLogs": false,
+        "captureErrors": false,
+        "cleanErrors": false,
+        "scramitize": false,
+        "sourcemaps": true
+    },
 });
+window.sj = scramjet;
+scramjet.init();
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        if (registrations.length === 0) {
+            try {
+                navigator.serviceWorker.register("sw.js").then(() => {
+                    console.log("Service worker registered.");
+                });
+            } catch (err) {
+                console.error("An error occurred while registering service worker:", err);
+            }
+        }
+    });
+}
+init();
